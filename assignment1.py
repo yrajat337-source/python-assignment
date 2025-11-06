deb0143c9606bb53eae460b0163d4846b2cc6d52
rajat.yadav <yrajat337@gmail.com>
2025-11-06 14:23:16 +0530

    Assignment1 completed

---
+++ assignment1.py
@@ -0,0 +1,17 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "Undefined (cannot divide by zero)"
+
+print("\nResults:")
+print(f"Addition: {num1} + {num2} = {addition}")
+print(f"Subtraction: {num1} - {num2} = {subtraction}")
+print(f"Multiplication: {num1} * {num2} = {multiplication}")
+print(f"Division: {num1} / {num2} = {division}")
