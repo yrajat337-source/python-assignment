8f046ddeeef97e0ec0dbd9f1e7b59158f8f652e2
rajat.yadav <yrajat337@gmail.com>
2025-11-08 23:48:12 +0530

    assignment3 completed

---
+++ assignment3.py
@@ -0,0 +1,20 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+num = int(input("Enter a number: "))
+print("The factorial of", num, "is:", factorial(num))
+import math
+
+num = float(input("Enter a number: "))
+
+sqrt_value = math.sqrt(num)
+log_value = math.log(num)
+sine_value = math.sin(num)
+
+print("Square root of", num, "is:", sqrt_value)
+print("Natural logarithm of", num, "is:", log_value)
+print("Sine of", num, "is:", sine_value)
+
